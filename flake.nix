5b794ad3a35667d876aa975b5679d9b3a765df7c
remimimimimi <remimimimimi@protonmail.com>
2022-12-25 15:09:06 +0200

    Start using flakes instead of inigo

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    idris-lang.url = "github:idris-lang/Idris2";
+    idris-lang.inputs.nixpkgs.follows = "nixpkgs";
+    # XXX: LUTI' HACK
+    idris2-aoc.url = "github:NobbZ/aoc-idr";
+    idris2-aoc.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, idris-lang, idris2-aoc }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        idrisPkgs = idris-lang.packages.${system};
+        idris2 = ((idris2-aoc.packages.x86_64-linux.idris2.override { withSource = true; }).overrideAttrs (old:
+          let
+            normalIdris = idris-lang.packages.${system}.idris2;
+          in {
+            version = normalIdris.version;
+            src = normalIdris.src;
+        }));
+      in {
+        devShells.default =
+          pkgs.mkShell { buildInputs = [ idris2 ]; };
+      });
+}
